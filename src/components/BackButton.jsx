5a8b9fd4bf23b8b2eb9c7c17bbb9057a103bfaf6
Jhersvin Villodas Quinto <jhersvinvillodasquinto@gmail.com>
2024-02-22 17:28:27 -0600

    First Commit

---
+++ src/components/BackButton.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import {BsArrowLeft} from 'react-icons/bs';
+
+function BackButton({destinations='/'}) {
+  return (
+    <div className='flex'>
+      <Link to={destinations} className='bg-sky-800 text-white px-4 py-1 rounded-lg w-fit'>
+        <BsArrowLeft className='text-2x1' />
+      </Link>BackButton</div>
+  )
+}
+
+export default BackButton
